338449fc2acaefd1d959a410b312f0d05e1cb8a4
Obafemi Joseph <obafedance@gmail.com>
2021-11-22 00:15:50 +0100

    fix: remove unused variables for build

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import homepageReducer from "./pages/HomePage/redux/reducer.redux";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { hashnodeApi } from "./utils/API_Handler";
